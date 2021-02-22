090c1bcd1f2c379772364943fe9b833b65c0255f
havenia <havenia09@gmail.com>
2021-02-22 21:34:11 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/', function(){
+//     echo 'Selamat Datang';
+// });
+
+Route::get('/', function(){
+    echo 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    echo 'NIM  : 1941720050 <br> Nama : Havenia Ventina Banjarnahor';
+});
+
+Route::get('/articles/{id}', function($id){
+    echo 'Halaman Artikel dengan id '.$id;
+});
